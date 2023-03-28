c012e3e280750bdf73338d534762cffe2490000a
WisnuKresnohadi <wisnukresnohadi@gmail.com>
2023-03-28 16:40:17 +0700

    modul_6

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
-<?php
-$t = date("H");
-if ($t < 20) {
-    # code...
-    echo "Have a good day";
-}
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <?php
+    $t = date("H");
+    if ($t < "20") {
+        # code...
+        echo "Have a good day";
+    } else {
+        # code...
+        echo "Have a good night";
+    }
+    ?>
+</body>
+
+</html>
